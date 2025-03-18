01bd345a950c416401a41b7091eee2c612742386
nastya <lobash.n@gmail.com>
2025-03-18 15:02:53 +0200

    to work

---
+++ src/components/App/App.jsx
@@ -0,0 +1,15 @@
+// import { useState } from 'react'
+import css from './App.module.css'
+
+export default function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+      <div className={css.container}>
+       <h1>DZ 5</h1>
+      </div>
+      
+  )
+}
+
+ 
